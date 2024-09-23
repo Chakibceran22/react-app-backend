84d4f1c897b8ba183290a5f325a8363e37fb672a
Chakibceran22 <grabachakib008@gmail.com>
2024-09-23 18:11:19 +0100

    hello im trying again

--- api/login.js
+++ api/login.js
@@ -3,6 +3,13 @@ import User from "../models/userModel.js";
 import express from "express";
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+    try {
+       res.send("Server is running");
+    } catch (error) {
+        res.status(404).send({ message: error.message });
+    }
+});
 router.post("/", async (req, res) => {
     try {
         const { username, password } = req.body;
